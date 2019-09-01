a351095820f169e4d79604f73463584c428c4186
ravibp <ravi.prasanna@infosys.com>
2019-09-01 12:42:06 +0530

    arcade game extended version started

--- js/engine.js
+++ js/engine.js
@@ -25,27 +25,22 @@ var Engine = (function(global) {
     ctx = canvas.getContext("2d"),
     lastTime;
 
-    ctx.font = "30px Arial";
-  ctx.fillText("Hello World", 10, 50);
-let points = 10;
-  canvas.width = 505;
-  canvas.height = 706;
-  ctx.font = "30px Arial";
-  ctx.fillText(points, 10, 50);
-  doc.body.appendChild(canvas);
- 
-  /* This function serves as the kickoff point for the game loop itself
-   * and handles properly calling the update and render methods.
-   */
-  function main() {
-    /* Get our time delta information which is required if your game
-     * requires smooth animation. Because everyone's computer processes
-     * instructions at different speeds we need a constant value that
-     * would be the same for everyone (regardless of how fast their
-     * computer is) - hurray time!
+    canvas.width = 505;
+    canvas.height = 606;
+    doc.body.appendChild(canvas);
+
+    /* This function serves as the kickoff point for the game loop itself
+     * and handles properly calling the update and render methods.
      */
-    var now = Date.now(),
-      dt = (now - lastTime) / 1000.0;
+    function main() {
+        /* Get our time delta information which is required if your game
+         * requires smooth animation. Because everyone's computer processes
+         * instructions at different speeds we need a constant value that
+         * would be the same for everyone (regardless of how fast their
+         * computer is) - hurray time!
+         */
+        var now = Date.now(),
+            dt = (now - lastTime) / 1000.0;
 
     /* Call our update/render functions, pass along the time delta to
      * our update function since it may be used for smooth animation.
@@ -74,19 +69,21 @@ let points = 10;
     main();
   }
 
-  /* This function is called by main (our game loop) and itself calls all
-   * of the functions which may need to update entity's data. Based on how
-   * you implement your collision detection (when two entities occupy the
-   * same space, for instance when your character should die), you may find
-   * the need to add an additional function call here. For now, we've left
-   * it commented out - you may or may not want to implement this
-   * functionality this way (you could just implement collision detection
-   * on the entities themselves within your app.js file).
-   */
-  function update(dt) {
-    updateEntities(dt);
-    // checkCollisions();
-  }
+    /* This function is called by main (our game loop) and itself calls all
+     * of the functions which may need to update entity's data. Based on how
+     * you implement your collision detection (when two entities occupy the
+     * same space, for instance when your character should die), you may find
+     * the need to add an additional function call here. For now, we've left
+     * it commented out - you may or may not want to implement this
+     * functionality this way (you could just implement collision detection
+     * on the entities themselves within your app.js file).
+     */
+    function update(dt) {
+        checkCollect();
+        updateEntities(dt);
+        checkCollisions();
+        
+    }
 
   /* This is called by the update function and loops through all of the
    * objects within your allEnemies array as defined in app.js and calls
@@ -102,28 +99,40 @@ let points = 10;
     player.update();
   }
 
-  /* This function initially draws the "game level", it will then call
-   * the renderEntities function. Remember, this function is called every
-   * game tick (or loop of the game engine) because that's how games work -
-   * they are flipbooks creating the illusion of animation but in reality
-   * they are just drawing the entire screen over and over.
-   */
-  function render() {
-    /* This array holds the relative URL to the image used
-     * for that particular row of the game level.
+    function checkCollisions(){
+        allEnemies.forEach(function (enemy) {
+            enemy.checkCol();
+        });
+    }
+
+    function checkCollect(){
+        allGems.forEach(function (gem) {
+            gem.gemCollect();
+        });
+    }
+    
+
+    /* This function initially draws the "game level", it will then call
+     * the renderEntities function. Remember, this function is called every
+     * game tick (or loop of the game engine) because that's how games work -
+     * they are flipbooks creating the illusion of animation but in reality
+     * they are just drawing the entire screen over and over.
      */
-    var rowImages = [
-        "images/water-block.png", // Top row is water
-        "images/stone-block.png", // Row 1 of 3 of stone
-        "images/stone-block.png", // Row 2 of 3 of stone
-        "images/stone-block.png", // Row 3 of 3 of stone
-        "images/grass-block.png", // Row 1 of 2 of grass
-        "images/grass-block.png" // Row 2 of 2 of grass
-      ],
-      numRows = 6,
-      numCols = 5,
-      row,
-      col;
+    function render() {
+        /* This array holds the relative URL to the image used
+         * for that particular row of the game level.
+         */
+        var rowImages = [
+                'images/water-block.png',   // Top row is water
+                'images/stone-block.png',   // Row 1 of 3 of stone
+                'images/stone-block.png',   // Row 2 of 3 of stone
+                'images/stone-block.png',   // Row 3 of 3 of stone
+                'images/grass-block.png',   // Row 1 of 2 of grass
+                'images/grass-block.png'    // Row 2 of 2 of grass
+            ],
+            numRows = 6,
+            numCols = 5,
+            row, col;
 
     /* Loop through the number of rows and columns we've defined above
      * and, using the rowImages array, draw the correct image for that
@@ -157,30 +166,45 @@ let points = 10;
       enemy.render();
     });
 
-    player.render();
-  }
+        player.render();
+        
+        allGems.forEach(function(gem){
+        gem.render();
+        });
 
-  /* This function does nothing but it could have been a good place to
-   * handle game reset states - maybe a new game menu or a game over screen
-   * those sorts of things. It's only called once by the init() method.
-   */
-  function reset() {
-    // noop
-  }
+        rock.render();
+    }
 
-  /* Go ahead and load all of the images we know we're going to need to
-   * draw our game level. Then set init as the callback method, so that when
-   * all of these images are properly loaded our game will start.
-   */
-  Resources.load([
-    "images/stone-block.png",
-    "images/water-block.png",
-    "images/grass-block.png",
-    "images/enemy-bug.png",
-    "images/char-pink-girl.png",
-    "images/char-boy.png"
-  ]);
-  Resources.onReady(init);
+    /* This function does nothing but it could have been a good place to
+     * handle game reset states - maybe a new game menu or a game over screen
+     * those sorts of things. It's only called once by the init() method.
+     */
+    function reset() {
+        // noop
+    }
+    /* Go ahead and load all of the images we know we're going to need to
+     * draw our game level. Then set init as the callback method, so that when
+     * all of these images are properly loaded our game will start.
+     */
+    Resources.load([
+        'images/stone-block.png',
+        'images/water-block.png',
+        'images/grass-block.png',
+        'images/enemy-bug.png',
+        'images/char-boy.png',
+        'images/Gem orange.png',
+        'images/Gem Green.png',
+        'images/Gem-Blue.png',
+        'images/Heart.png',
+        'images/star.png',
+        'images/rock.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png'
+    
+    ]);
+    Resources.onReady(init);
 
   /* Assign the canvas' context object to the global variable (the window
    * object when run in a browser) so that developers can use it more easily
